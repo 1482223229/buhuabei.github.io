ff986c6e5117dcba7668ad4b6c4d07120b638f85
hubei bu <buhuabei229@163.com>
2023-02-27 12:56:05 +0800

    chore: 修改vue.config配置（publicPath）

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,8 @@ const path = require("path");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/io" : "/",
+  // publicPath: process.env.NODE_ENV === "production" ? "/io" : "/",
+  publicPath: "/",
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
