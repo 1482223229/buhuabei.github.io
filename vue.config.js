c8a366d920dffdb9a2becd11732990e815d3420d
hubei bu <buhuabei229@163.com>
2024-02-28 00:56:55 +0800

    fix: 修改打包路径

--- vue.config.js
+++ vue.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 module.exports = {
-  // publicPath: process.env.NODE_ENV === "production" ? "/io" : "/",
-  publicPath: "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/io" : "/",
+  // publicPath: "/",
   pluginOptions: {
     "style-resources-loader": {
       preProcessor: "less",
